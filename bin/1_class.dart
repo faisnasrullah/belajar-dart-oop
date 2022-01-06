cde4ad82e27f42f40da25622599d84b6bb5d1186
Fais Nasrullah <fais@automatalabs.co>
2022-01-06 21:14:55 +0700

    Class Field di Dart

--- bin/1_class.dart
+++ bin/1_class.dart
@@ -6,13 +6,24 @@ class Person {
    * penamanaan class menggunakan PascalCase
   */
 
+  // field
+  String name = 'Guest';
+  String? address; // bisa atau boleh null / nullable
+  final String country = 'Indonesia'; // tidak bisa diubah
+
 }
 
 void main() {
   // Object
   var person1 = Person();
-  var person2 = Person();
+  person1.name = 'Fais Nasrullah'; // tidak bisa diisi null
+  person1.address = 'Depok';
+  // person1.country = 'Singapore'; // tidak bisa mengubah final field
 
-  print(person1);
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+
+  var person2 = Person();
   print(person2);
 }
--- bin/class.dart
+++
@@ -1,9 +0,0 @@
-/// Dokumentasi belajar class pada dart
-
-class Person {
-  /** 
-   * menggunakan kata kunci class
-   * penamanaan class menggunakan PascalCase
-  */
-
-}
