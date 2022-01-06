6185946ee31e7bc85ffb9fbee9437315dd9608dd
Fais Nasrullah <fais@automatalabs.co>
2022-01-06 18:16:55 +0700

    Object di Dart

---
+++ bin/1_class.dart
@@ -0,0 +1,18 @@
+/// Dokumentasi belajar class pada dart
+
+class Person {
+  /** 
+   * menggunakan kata kunci class
+   * penamanaan class menggunakan PascalCase
+  */
+
+}
+
+void main() {
+  // Object
+  var person1 = Person();
+  var person2 = Person();
+
+  print(person1);
+  print(person2);
+}
